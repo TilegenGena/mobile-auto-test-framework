51575f81836f604e0ba5860b2946cd1245838625
TilegenGena <tn.asankulov@gmail.com>
2025-12-26 16:51:51 +0500

    Base page and wait helpers

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected AndroidDriver driver;
+    protected WebDriverWait wait;
+
+    protected BasePage(AndroidDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected void waitAndClick(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    protected void waitAndSendKeys(By locator, String text) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
+    }
+
+    protected boolean isDisplayed(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).isDisplayed();
+    }
+}
